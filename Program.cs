a576d8f0f75a9f673604b0b207a6db5f3a647ade
Сергей <seregabur@live.ru>
2022-04-19 01:05:36 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Function4
 {
@@ -23,21 +24,124 @@ namespace Function4
                               {"#                #                #" },
                               {"#       ####################      #" },
                               {"#                                 #" },
-                              {"###################################" },};
+                              {"###################################" }};
 
-            MapVisualization(map);
+            char[,] CharMap = CreateCharMap(map);
+           
+            MapVisualization(CharMap);
 
-            Console.ReadKey();
-        }
+            int positionX = 1;//горизонт
+            int positionY = 1;//вертикал
+            int movX = 0;
+            int movY = 0;
+            int sleep = 100;
 
-        static void MapVisualization(string[,] map)
-        {
-            foreach(string str in map)
+            char plaer = '@';
+
+            Console.CursorVisible = false;
+            PlaerMovement(ref positionX, ref  positionY, movX, movY,plaer, CharMap);
+
+            while (true)
             {
-                Console.Write(str + "\n");
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+
+                            movY = -1; movX = 0;
+                            PlaerMovement(ref positionX, ref positionY, movX, movY, plaer, CharMap);
+
+                            break;
+                        case ConsoleKey.DownArrow:
+
+                            movY = 1;movX = 0;
+                            PlaerMovement(ref positionX, ref positionY, movX, movY, plaer, CharMap);
+
+                            break;
+                        case ConsoleKey.LeftArrow:
+
+                            movX = -1;movY = 0;
+                            PlaerMovement(ref positionX, ref positionY, movX, movY, plaer, CharMap);
+
+                            break;
+                        case ConsoleKey.RightArrow:
+
+                            movX = 1;movY = 0;
+                            PlaerMovement(ref positionX, ref positionY, movX, movY, plaer, CharMap);
+
+                            break;
+                    }
+
+                }
+               Thread.Sleep(sleep);
+              // Console.ReadKey();
             }
         }
 
+        static void PlaerMovement(ref int X, ref int Y, int movX, int movY, char plaer, char[,] map)
+        {
+
+
+            Console.SetCursorPosition(X, Y);
+            Console.Write(" ");
+
+            X += movX;
+            Y += movY;
+
+            char stopMovement = '#';
+            if(X<1)
+            { X = 1; }
+
+            if(Y<1)
+            { Y = 1; }
+
+            Console.SetCursorPosition(1, 13);///////
+            Console.Write("X=" + X + " Y=" + Y+" map="+"["+map[X,Y]+"]");//////
+
+            if (map[X,Y]!=stopMovement)
+            {
+                Console.SetCursorPosition(X, Y);
+                Console.Write(Y);
+            }else
+            {
+               
+                Console.SetCursorPosition(X-movX, Y-movY);
+                Console.Write(plaer);
+            }
+
+        }
+
+        static void MapVisualization(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+
+                    Console.Write(map[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static char[,] CreateCharMap(string[,] map)
+        {
+            char[,] charMap= new char[map.GetLength(0), map[0, 0].Length];
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                string tt = map[i, 0];
+
+                for (int j = 0; j < tt.Length; j++)
+                {
+                    charMap[i, j] = tt[j];
+                }
+            }
+
+            return charMap;
+        }
 
     }
 }
